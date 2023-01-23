e8c9c8b9d1f229ff4b8d01847b5fdcab52b94467
Thomas Fuhriman <seniorswanky@icloud.com>
2023-01-23 16:51:25 -0700

    commit

--- week2/scoreboard/app.js
+++ week2/scoreboard/app.js
@@ -4,9 +4,11 @@ let away = 0
 
 function reset(){
     let scorecard = document.getElementById("home-score");
+    let scorecardAway = document.getElementById("visitor-score");
     home = 0;
+    away = 0;
     scorecard.innerText = home;
-    scorecard.innerText = away;
+    scorecardAway.innerText = away;
 }
 
 
@@ -20,6 +22,20 @@ function plusOne(){
 function plusOneAway(){
     let scorecard = document.getElementById("visitor-score");
     away ++;
+
+    scorecard.innerText = away.toString();
+
+}
+function plusThree(){
+    let scorecard = document.getElementById("home-score");
+    home += 3;
+    
+    scorecard.innerText = home.toString();
+
+}
+function plusThreeAway(){
+    let scorecard = document.getElementById("visitor-score");
+    away +=3;
 
     scorecard.innerText = away.toString();
 
